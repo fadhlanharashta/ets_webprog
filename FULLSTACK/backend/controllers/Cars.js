fcdf1fda9f8811476933623e403fcd09cbbcfbf4
fadhlanharahshta <works.fadhlan@example.com>
2023-03-30 21:36:07 +0700

    commit 1

---
+++ FULLSTACK/backend/controllers/Cars.js
@@ -0,0 +1,22 @@
+import Cars  from "../models/CarModel.js";
+
+export const getCars = (req, res) =>{
+
+}
+
+export const getCarsById = (req, res) =>{
+
+}
+
+export const createCars = (req, res) =>{
+
+}
+
+export const updateCars = (req, res) =>{
+
+}
+
+export const deleteCars = (req, res) =>{
+
+}
+
